9766073f7a8bb8d967991186737c5f8683289a5a
Aphcity <chalk.talisman@gmail.com>
2025-04-30 15:29:58 -0400

    ✨ feat(phash): enhance perceptual hash calculation with SHA3-256 and Hamming distance
    * Added `Hamming_distance` function to compute similarity between hashes.
    * Integrated SHA3-256 hashing for original images.
    * Updated example usage to demonstrate batch processing and distance calculations.
    * Included new test images for comprehensive testing.

--- phash.py
+++ phash.py
@@ -1,8 +1,12 @@
+import hashlib
+
 import PIL
 import numpy as np
 import cv2
 import torch
 from PIL import Image
+import time
+import os
 def calculate_phash(image_paths, hash_size=8, path=True):
     """
     Calculate the perceptual hash (pHash) of one or more images.
@@ -58,15 +62,49 @@ def calculate_phash(image_paths, hash_size=8, path=True):
     else:
         raise ValueError("image_paths must be a string or a list of strings.")
 
+def Hamming_distance(hash1,hash2):
+    num = 0
+    for index in range(len(hash1)):
+        if hash1[index] != hash2[index]:
+            num += 1
+    return num
 
 # Example usage
 if __name__ == "__main__":
     # Single image
-    single_image_path = "example.jpg"
-    phash = calculate_phash(single_image_path)
-    print(f"Perceptual Hash (single image): {phash}")
+    # single_image_path = "example.jpg"
+    # phash = calculate_phash(single_image_path)
+    # print(f"Perceptual Hash (single image): {phash}")
     
     # Batch of images
-    batch_image_paths = ["example1.jpg", "example2.jpg", "example3.jpg"]
+    batch_image_paths = ["test phash/im1.jpg", "test phash/im1_blur.jpg", "test phash/im1_text.jpg", "test phash/im1_crop.jpg", "test phash/im1_dot.jpg"]
     phashes = calculate_phash(batch_image_paths)
-    print(f"Perceptual Hashes (batch): {phashes}")
+    # calculate the sha3 hash of the image
+    sha3_hash = [hashlib.sha3_256(open(batch_image_paths[0], 'rb').read()).hexdigest(),
+                 hashlib.sha3_256(open(batch_image_paths[1], 'rb').read()).hexdigest(),
+                 hashlib.sha3_256(open(batch_image_paths[2], 'rb').read()).hexdigest(),
+                 hashlib.sha3_256(open(batch_image_paths[3], 'rb').read()).hexdigest(),
+                 hashlib.sha3_256(open(batch_image_paths[4], 'rb').read()).hexdigest()]
+    print(f"Original SHA3-256 Hash: {sha3_hash[0]}")
+
+    start1 = time.time()
+    blur_dist = Hamming_distance(phashes[0], phashes[1])
+    end1 = time.time()
+    print('blur_dist is '+'%d' % blur_dist + ', similarity is ' +'%f' % (1 - blur_dist * 1.0 / 64) + ', time is ' +'%f' % (end1-start1) + ', phash is ' + phashes[1]+ ', sha3 is ' + sha3_hash[1])
+
+    start2 = time.time()
+    text_dist = Hamming_distance(phashes[0], phashes[2])
+    end2 = time.time()
+    print('text_dist is '+'%d' % text_dist + ', similarity is ' +'%f' % (1 - text_dist * 1.0 / 64) + ', time is ' +'%f' % (end2-start2) + ', phash is ' + phashes[2]+ ', sha3 is ' + sha3_hash[2])
+
+    start3 = time.time()
+    crop_dist = Hamming_distance(phashes[0], phashes[3])
+    end3 = time.time()
+    print('crop_dist is '+'%d' % crop_dist + ', similarity is ' +'%f' % (1 - crop_dist * 1.0 / 64) + ', time is ' +'%f' % (end3-start3) + ', phash is ' + phashes[3]+ ', sha3 is ' + sha3_hash[3])
+
+    start4 = time.time()
+    dot_dist = Hamming_distance(phashes[0], phashes[4])
+    end4 = time.time()
+    print('dot_dist is '+'%d' % dot_dist + ', similarity is ' +'%f' % (1 - dot_dist * 1.0 / 64) + ', time is ' +'%f' % (end4-start4) + ', phash is ' + phashes[4]+ ', sha3 is ' + sha3_hash[4])
+    # Uncomment the following line to see the perceptual hashes for the batch of images
+    # print(f"Perceptual Hashes (batch): {phashes}")
